b0d06439e9dab7764feb432483653e5fee71c957
Lekshmi J <lekshmi.j@seqato.com>
2021-09-08 21:40:53 +0530

    modified file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CareerComponent } from './careers/careers.component'
+import { CareerComponent } from './careers/careers.component';
+import { EmployeeComponent } from './employees/employees.component'
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeComponent },
   { path: 'careers', component: CareerComponent },
   { path: 'home', component: HomeComponent }
 ];
@@ -22,7 +25,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
-    CareerComponent
+    CareerComponent,
+    EmployeeComponent
   ],
   bootstrap: [AppComponent]
 })
